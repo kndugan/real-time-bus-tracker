a931a7ff5102c71786b706f92c0fd4e357ef247b
Kristine Dugan <kndugan@gmail.com>
2022-03-18 12:05:59 -1000

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Real Time Bus Tracker for Kauai Hawaii Bus Stops
+
+## Description
+
+Demonstrate web application using JavaScript, HTML, and CSS to move Map Marker of Bus Stops on Kauai, Hawaii between Lihue and Hanalei.
+
+Use: https://www.mapbox.com/
+JavaScript: https://api.mapbox.com/mapbox-gl-js/v1.11.0/mapbox-gl.js
+Stylesheet: https://api.mapbox.com/mapbox-gl-js/v1.11.0/mapbox-gl.css
+
+The starter code was provided by MIT xPro.
+
+## Roadmap of Fugure Improvements
+
+Ideas include adding other bus routes to other areas of the island.
+
+## Questions or Comments?
+
+Connect with me on Twitter at @KristineDugan <https://twitter.com/kristinedugan> or LinkedIn @KristineDugan <https://linkedin.com/in/kristinedugan>
+
+## License Information
+
+Create yourself an Access Key from https://www.mapbox.com/.
+Some provided materials from the lecture videos and from assignment solutions are used in this project.
+Some implementations are derived from the provided materials of the course.
---
+++ app.js
@@ -0,0 +1,50 @@
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoia25kdWdhbiIsImEiOiJjbDBxYWw2ZjkxMWVmM2JvN2VhM2Q2OGp6In0.nnTtL798NQP2aQodAvVwkw";
+
+// Map settings and styles from Map Box
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/navigation-day-v1",
+  center: [-159.530856, 22.047639],
+  zoom: 9.5,
+});
+
+// center marker in Lihue
+var marker = new mapboxgl.Marker()
+  .setLngLat([-159.369318, 21.976709])
+  .addTo(map);
+
+// bus stops between Lihue, Hawaii and Hanalei, Hawaii on Kauai
+const busStops = [
+  [-159.3677856, 21.9754634], // 1 Eiwa St.
+  [-159.3668104, 21.9844342], // 2 Across from Fish Express
+  [-159.3571743, 21.9934489], // 3 Kuhio Hwy/Laukona Rd
+  [-159.3528568, 21.9958407], // 4 Peter Rayno Park
+  [-159.3324778, 22.053197], // 5 Waipouli Courtyard
+  [-159.3204229, 22.0618377], // 6 Waipouli Beach Resort
+  [-159.3194719, 22.0705925], // 7 Kapaa Hongwanji
+  [-159.3192003, 22.073569], // 8 Pono Kai
+  [-159.316274, 22.0778136], // 9 Kapaa Library
+  [-159.3144033, 22.0798133], // 10 Kapaa Neighborhood Ctr
+  [-159.303727, 22.103678], // 11 Kealia Beach
+  [-159.3053204, 22.13792], // 12 Kukuihale Rd
+  [-159.3149028, 22.1444301], // 13 Anahola Post Office
+  [-159.3309907, 22.1796823], // 14 Moloaa Fruit Stand
+  [-159.4119149, 22.2072951], // 15 Kilauea Food Mart
+  [-159.415232, 22.210796], // 16 Anaina Hou
+  [-159.4741024, 22.2131026], // 17 Princeville Shopping Ctr
+  [-159.4975487, 22.2026678], // 18 Hanalei Post Office
+  [-159.5007317, 22.2017608], // 19 Hanalei Neighborhood Ctr
+];
+
+var counter = 0;
+
+// Move in increments through bus stops
+function move() {
+  setTimeout(() => {
+    if (counter >= busStops.length) return;
+    marker.setLngLat(busStops[counter]);
+    counter++;
+    move();
+  }, 1000);
+}
